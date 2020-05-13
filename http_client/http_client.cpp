7572bd75c6160996de2470db5a7d5531ca273d6f
82786333@qq.com <82786333@qq.com>
2020-05-13 20:10:52 +0800

    http client win32

---
+++ http_client/http_client.cpp
@@ -0,0 +1,24 @@
+﻿// http_client.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+#include "httpclient.hpp"
+
+int main()
+{
+    std::cout << "Hello World!\n";
+	System::Net::HttpGobal::Initialize();
+
+   std::string uri = "https://www.baidu.com";
+   System::Net::HttpWebRequest* req = System::Net::HttpWebRequest::Create(uri);
+   System::Net::HttpWebResponse* resp = req->GetResponse();
+
+   std::cout << resp->GetHeader(System::Net::HttpHeader::ContentLength) << std::endl;
+   std::cout << resp->GetResponseStream().str() << std::endl;
+
+   std::string str = resp->GetResponseStream().str();
+   std::cout << str << std::endl;
+   System::Net::HttpGobal::Destroy();
+}
+
